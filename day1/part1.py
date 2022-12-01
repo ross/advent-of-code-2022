bc1d9a35e3d84a4dc5a8f362cb080691f74102a3
Ross McFarland <rwmcfa1@gmail.com>
2022-12-01 06:52:21 -0800

    day1

---
+++ day1/part1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from pprint import pprint
+from sys import stdin
+
+elves = []
+calories = 0
+for row in stdin:
+    row = row.strip()
+    if not row:
+        # next elf, regardless of what they're carring
+        elves.append(calories)
+        calories = 0
+    else:
+        calories += int(row)
+# last elve, doesn't matter if they're carrying anything we'll count zeros
+elves.append(calories)
+
+most = max(elves)
+
+print(most)
---
+++ day1/part2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from pprint import pprint
+from sys import stdin
+
+elves = []
+calories = 0
+for row in stdin:
+    row = row.strip()
+    if not row:
+        # next elf, regardless of what they're carring
+        elves.append(calories)
+        calories = 0
+    else:
+        calories += int(row)
+# last elve, doesn't matter if they're carrying anything we'll count zeros
+elves.append(calories)
+# sort them most to least
+elves.sort(reverse=True)
+
+calories = elves[0] + elves[1] + elves[2]
+
+print(calories)
