70ac507144eb2f27b21cc7b0813c89d69ab0c99d
Ross McFarland <rwmcfa1@gmail.com>
2022-12-04 06:47:45 -0800

    don't need to check if b starts before a ends, if the it ends before

--- day4/part1.py
+++ day4/part1.py
@@ -11,10 +11,10 @@ for row in stdin:
     a_start, a_end = (int(v) for v in a.split('-'))
     # find the seconds start and end
     b_start, b_end = (int(v) for v in b.split('-'))
-    if a_start <= b_start and b_start <= a_end and b_end <= a_end:
+    if a_start <= b_start and b_end <= a_end:
         # b is inside of or equal to a
         contained += 1
-    elif b_start <= a_start and a_start <= b_end and a_end <= b_end:
+    elif b_start <= a_start and a_end <= b_end:
         # a is inside of or equal to b
         contained += 1
 
