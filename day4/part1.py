a6aa35346f4ebdb36d5d852b63039e364d586be4
Ross McFarland <rwmcfa1@gmail.com>
2022-12-04 06:36:26 -0800

    day4 comments

--- day4/part1.py
+++ day4/part1.py
@@ -5,8 +5,11 @@ from sys import stdin
 contained = 0
 for row in stdin:
     row = row.strip()
+    # break into two parts
     a, b = row.split(',')
+    # find the first's start and end
     a_start, a_end = (int(v) for v in a.split('-'))
+    # find the seconds start and end
     b_start, b_end = (int(v) for v in b.split('-'))
     if a_start <= b_start and b_start <= a_end and b_end <= a_end:
         # b is inside of or equal to a
--- day4/part2.py
+++ day4/part2.py
@@ -2,17 +2,20 @@
 
 from sys import stdin
 
-contained = 0
+overlaps = 0
 for row in stdin:
     row = row.strip()
+    # break into two parts
     a, b = row.split(',')
+    # find the first's start and end
     a_start, a_end = (int(v) for v in a.split('-'))
+    # find the seconds start and end
     b_start, b_end = (int(v) for v in b.split('-'))
     if a_start <= b_start and b_start <= a_end:
         # b starts at or after a and before a ends, there's some overlap
-        contained += 1
+        overlaps += 1
     elif b_start <= a_start and a_start <= b_end:
         # a starts at or after b and before b ends, there's some overlap
-        contained += 1
+        overlaps += 1
 
-print(contained)
+print(overlaps)
